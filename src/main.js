7933d92e0653eb4f7200b4ffea3e0079471348d8
wrk <wangruikaihaha@163.com>
2022-12-05 16:29:03 +0800

    项目初始化-移动端rem适配和封装请求模块

--- src/main.js
+++ src/main.js
@@ -2,13 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载全局样式
 import './styles/index.less'
 
+// 加载vant 核心组件库
 import Vant from 'vant'
+// 加载vant全局样式
 import 'vant/lib/index.css'
+
+// 动态设置 rem 基准值
+import 'amfe-flexible'
 
 Vue.config.productionTip = false
 
+// 注册使用 Vant 组件库
 Vue.use(Vant)
 
 new Vue({
